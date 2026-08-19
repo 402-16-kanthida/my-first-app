a2d86047ef3723595e2c53f1f86f30bf893aa7d9
402-16-kanthida <54278@yupparaj.ac.th>
2026-08-19 15:33:55 +0700

    Update vocab_game.py

--- vocab_game.py
+++ vocab_game.py
@@ -44,14 +44,14 @@ def show_result_dialog(ans1, ans2):
         st.error(f"❌ ข้อ 2: ยังไม่ถูกต้อง (คุณตอบ '{u_ans2}')")
 
     # ตรวจข้อ 3
-    if u_ans1 == "grass":
+    if u_ans1 == "bananas":
         st.success("✅ ข้อ 3: ถูกต้อง")
         score += 1
     else:
         st.error(f"❌ ข้อ 3: ยังไม่ถูกต้อง (คุณตอบ '{u_ans3}')")
       
  # ตรวจข้อ 4
-    if u_ans1 == "banana":
+    if u_ans1 == "phone":
         st.success("✅ ข้อ 4: ถูกต้อง")
         score += 1
     else:
